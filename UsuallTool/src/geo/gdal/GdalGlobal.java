976728e390b9b6380a2f9266b42f78357fc1d389
alter <alter@alter-PC>
2019-03-07 18:02:55 +0800

    20190307

--- UsuallTool/src/geo/gdal/GdalGlobal.java
+++ UsuallTool/src/geo/gdal/GdalGlobal.java
@@ -1,6 +1,47 @@
 package geo.gdal;
 
+import java.awt.geom.Path2D;
+import java.awt.geom.PathIterator;
+
+import org.gdal.ogr.Geometry;
+
 public class GdalGlobal {
-	
+
 	public static String gdalBinFolder = "C:\\code\\JAVA library\\gdal\\bin\\";
+
+	public static Path2D GeomertyToPath2D(Geometry geometry) {
+		Path2D outPath = new Path2D.Double();
+		double[][] points = geometry.GetBoundary().GetPoints();
+
+		outPath.moveTo(points[0][0], points[0][1]);
+		for (int index = 1; index < points.length; index++) {
+			outPath.lineTo(points[index][0], points[index][1]);
+		}
+		return outPath;
+	}
+
+	public static Geometry Path2DToGeometry(Path2D path) {
+		PathIterator temptPathIteratore = path.getPathIterator(null);
+		float coordinate[] = new float[2];
+
+		// start coordinate
+		temptPathIteratore.currentSegment(coordinate);
+		double startX = coordinate[0];
+		double startY = coordinate[1];
+
+		// output geometry ,start point
+		StringBuilder sb = new StringBuilder();
+		sb.append("{\"type\" : \"Polygon\" , \"coordinates\" : [[");
+		sb.append("[" + startX + "," + startY + "],");
+		temptPathIteratore.next();
+
+		for (; !temptPathIteratore.isDone(); temptPathIteratore.next()) {
+			temptPathIteratore.currentSegment(coordinate);
+			sb.append("[" + coordinate[0] + "," + coordinate[1] + "],");
+		}
+		sb.append("[" + startX + "," + startY + "] ]] }");
+
+		return Geometry.CreateFromJson(sb.toString());
+	}
+
 }
--- UsuallTool/src/testFolder/test.java
+++ UsuallTool/src/testFolder/test.java
@@ -1,10 +1,5 @@
 package testFolder;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-
 import org.gdal.ogr.Geometry;
 
 import geo.gdal.SpatialFileTranslater;
@@ -15,29 +10,12 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String fileList[] = new String[] { "H:/SHP/南崁集水區(下水道)20190205.shp", "H:/SHP/97南崁集水分區(明渠)2.0.shp",
-				"H:/SHP/97東門溪集水分區(明渠)(222個分區).shp" };
+		SpatialReader spReader = new SpatialReader("H:/SHP/dongman_merge(Sobek).shp");
+		Geometry polygon = spReader.getGeometryList().get(0);
 
-		SpatialWriter spWrite = new SpatialWriter();
-		Map<String, String> attributeTitle = new TreeMap<>();
-
-		attributeTitle.put("Polygon_ID", "String");
-		spWrite.setCoordinateSystem(SpatialWriter.TWD97_121);
-		spWrite.setField(attributeTitle);
-
-		for (String fileAdd : fileList) {
-			SpatialReader shp = new SpatialReader(fileAdd,"UTF-8");
-			List<Geometry> temptList = shp.getGeometryList();
-			List<Map<String, String>> temptAttribute = shp.getAttributeTable();
-
-			for (int index = 0; index < temptList.size(); index++) {
-				Map<String, Object> attributeValue = new TreeMap<>();
-				attributeValue.put("Polygon_ID", temptAttribute.get(index).get("Polygon_ID"));
-				spWrite.addFeature(temptList.get(index), attributeValue);
-			}
+		for (double[] point : polygon.GetBoundary().GetPoints()) {
+			System.out.println(point[0] + "\t" + point[1]);
 		}
-
-		spWrite.saveAsShp("H:/SHP/dongman_merge(Sobek).shp");
 
 	}
 
