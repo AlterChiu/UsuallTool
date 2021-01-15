0347dec1131edcb763677296600def1cc1258a0d
AlterChiu <andy950702@gmail.com>
2021-01-15 17:23:23 +0800

    20210115

--- UsuallTool/src/testFolder/testAtCommon.java
+++ UsuallTool/src/testFolder/testAtCommon.java
@@ -8,6 +8,8 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.gdal.gdal.gdal;
+
 import Microsoft.Office.PowerPoint.PPTBasicControl;
 import Microsoft.Office.PowerPoint.PPTBasicControl.PPTTextBox;
 import https.Rest.geo.wms.WMSBasicControl;
@@ -19,22 +21,14 @@ import usualTool.DateValueStorage;
 
 public class testAtCommon {
 	public static void main(String[] args) throws Exception {
-		String fileAdd = "C:\\Users\\alter\\Downloads\\新竹水利會-水資源聯合運用模組基礎資料 - 去除最大值 - 時間.txt";
-		String saceAdd = "C:\\Users\\alter\\Downloads\\新竹水利會-水資源聯合運用模組基礎資料 - 平均到天.txt";
-
-		DateValueStorage dataStorage = new DateValueStorage(new AtFileReader(fileAdd).getContent("\t"),
-				"yyyy/MM/dd HH:mm");
-
-		dataStorage.setStartDate("2015/01/01", "yyyy/MM/dd");
-		dataStorage.setEndDate("2020/04/30", "yyyy/MM/dd");
-		dataStorage.saveToDay(saceAdd, StaticsModel.getMean);
+		gdal.AllRegister();
 	}
 
 	public static void fromNegetiveToUnrealable() throws IOException {
 		double[] maxValues = new double[] { 28.35, 249.03, 35.3, 101.37, 53.42, 1.045, 2.6986, 1.463, 2.777, 0.71219,
 				1.203, 2.29, 0.176, 2.94, 0.455, 1.17172, 0.176, 1.936036, 0.117, 1.28416 };
 
-		String fileAdd = "C:\\Users\\alter\\Downloads\\新竹水利會-水資源聯合運用模組基礎資料 - 去除負值.txt";
+		String fileAdd = "C:\\/Users\\alter\\Downloads\\新竹水利會-水資源聯合運用模組基礎資料 - 去除負值.txt";
 		String saveAdd = "C:\\Users\\alter\\Downloads\\新竹水利會-水資源聯合運用模組基礎資料 - 去除最大值.txt";
 
 		List<String[]> outList = new ArrayList<>();
